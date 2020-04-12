d91ecd09b2b4544171ea70d8303ddfdd587b3568
serapaydogdu <serapaydgdu@gmail.com>
2020-04-12 03:34:13 +0300

    14. videoya kadar tamamlandı. En son "Contact Form Bize Ulaşın" kısmı oluşturuldu.

--- home/models.py
+++ home/models.py
@@ -2,6 +2,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
+from django.forms import ModelForm, TextInput, Textarea
+
 
 class Setting(models.Model):
     STATUS = (
@@ -34,4 +36,32 @@ class Setting(models.Model):
     def __str__(self):
         return self.title
 
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+    )
+    name= models.CharField(blank=True, max_length=20)
+    email= models.CharField(blank=True, max_length=50)
+    subject= models.CharField(blank=True, max_length=50)
+    message= models.CharField(blank=True, max_length=255)
+    status= models.CharField(max_length=10, choices=STATUS, default='New')
+    ip= models.CharField(blank=True, max_length=20)
+    note= models.CharField(blank=True, max_length=100)
+    create_at= models.DateTimeField(auto_now_add=True)
+    update_at= models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name'       : TextInput(attrs={'class': 'common-input mb-20 form-control','placeholder':'Name & Surname' ,'required':' ', 'type':'text','onfocus':'this.placeholder = '''}),
+            'subject'    : TextInput(attrs={'class': 'common-input mb-20 form-control', 'placeholder': 'Subject'}),
+            'email'      : TextInput(attrs={'class': 'common-input mb-20 form-control', 'placeholder': 'Email Address','required':' ', 'type':'email','pattern':'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$'}),
+            'message'    : Textarea(attrs={'class': 'common-input mb-20 form-control', 'placeholder': 'Your Message', 'row':'5'}),
+        }
--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
+
 
 def index(request):       #setting ayarlarını getirecek . artık sitede görünecekler title ı vs.
     setting = Setting.objects.get(pk=1)    #templates in index inde değişiklik yapıldı.
-    context = {'setting': setting, 'page':'home'}
+    context = {'setting': setting, 'page': 'home'}
     return render(request, 'index.html', context)  #index.htmle gönderdik.
 
 def hakkimizda(request):
@@ -20,6 +22,19 @@ def references(request):
     return render(request, 'references.html', context)
 
 def contact(request):
+    if request.method =='POST':                       #FORM POSTT EDİLMİŞ İSE
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()                   #Model ile bağlantı kuruldu.
+            data.name = form.cleaned_data['name']     #formdan bilgiyi aldık.
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()                                #veritabanına kaydettikk.
+            messages.success(request, "Mesajınız başarılı bir şekilde gönderilmiştir. Teşekkür ederiz.")
+            return HttpResponseRedirect('/contact')
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'contact.html', context)
